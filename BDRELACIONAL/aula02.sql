dc5d91226ec9b8a8915910f0266ae6188190a0e0
filipejonatas <jonatas.filipe.ferreira@gmail.com>
2024-02-20 22:47:35 -0300

    aula2bdrelacional

---
+++ BDRELACIONAL/aula02.sql
@@ -0,0 +1,35 @@
+-- Criar banco de dados bd_sistema_bancario
+
+create database bd_sistema_bancario;
+
+-- Criar 5 tabelas quaiquer
+
+
+postgres=# create table cliente_table1 (cliente_coluna1 char, cliente_coluna2 char);
+CREATE TABLE
+postgres=# create table cliente_table2 (cliente_coluna1 char, cliente_coluna2 char);
+CREATE TABLE
+postgres=# create table cliente_table3 (cliente_coluna1 char, cliente_coluna2 char);
+CREATE TABLE
+postgres=# create table cliente_table4 (cliente_coluna1 char, cliente_coluna2 char);
+CREATE TABLE
+postgres=# create table cliente_table5 (cliente_coluna1 char, cliente_coluna2 char);
+CREATE TABLE
+postgres=#
+
+
+-- Criar tabelas conforme MER (Sistema Bancario):
+
+
+
+-- Deletar as 5 tabelas criadas aleatoriamente
+
+
+
+-- alterar as tabelas conforme o MER atualizado
+
+
+
+-- Criar um esquema com o seu nome
+
+
